e55e5c3d8a0866fca3bc30cfb102afa7dc2bb663
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-07-25 15:43:34 +0200

    feat: first exercise run

--- JSnack1/README.md
+++ JSnack1/README.md
@@ -5,3 +5,7 @@
 Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
 ## SVOLGIMENTO
+
+- chiedi all'utente un numero
+  - **SE** il numero inserito è pari stampa il numero
+  - **ALTRIMENTI** aggiungi 1 al numero dato e stampa quel numero
--- JSnack1/main.js
+++ JSnack1/main.js
@@ -1,1 +1,13 @@
-console.log("js ok");
+//* chiedi all'utente un numero
+const userNumber = parseInt(prompt("Inserisci un numero"));
+
+//* **SE** il numero inserito è pari stampa il numero
+
+if (userNumber % 2 == 0) {
+  console.log(userNumber);
+}
+
+//* **ALTRIMENTI** aggiungi 1 al numero dato e stampa quel numero
+else {
+  console.log(userNumber + 1);
+}
