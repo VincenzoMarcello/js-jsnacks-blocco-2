1290624272ba00b24a09d3002b545327380aceaf
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-07-25 17:21:49 +0200

    feat: add third exercise complete

--- JSnack3/README.md
+++ JSnack3/README.md
@@ -5,3 +5,9 @@
 Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 
 ## SVOLGIMENTO
+
+- creo una lista di numeri
+- inizializzo una variabile d'appoggio dove conservare la somma dei dispari
+- creo un ciclo che controlli i numeri
+  - **SE** l'indice è dispari, aggiungiamo l'elemento alla somma
+- stampiamo la somma dei numeri dispari
--- JSnack3/main.js
+++ JSnack3/main.js
@@ -1,1 +1,16 @@
-console.log("js ok");
+//* creo una lista di numeri
+const numbers = [3, 2, 8, 4, 9, 7];
+
+//* inizializzo una variabile d'appoggio dove conservare la somma dei dispari
+let numbersOddTotal = 0;
+
+//* creo un ciclo che controlli i numeri
+for (let i = 0; i < numbers.length; i++) {
+  //* **SE** l'indice è dispari, aggiungiamo l'elemento alla somma
+  if (i % 2 !== 0) {
+    numbersOddTotal += numbers[i];
+  }
+}
+
+//* stampiamo la somma dei numeri dispari
+console.log("La somma dei numeri in posizione dispari è: " + numbersOddTotal);
