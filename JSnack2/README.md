4985581200a40fc29b57db34118c086131886f65
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-07-25 16:13:13 +0200

    feat: add second exercise complete

--- JSnack2/README.md
+++ JSnack2/README.md
@@ -5,3 +5,8 @@
 Generatore di “nomi cognomi” casuali: il Grande Gatsby ha una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
 ## SVOLGIMENTO
+
+- creo una lista di nomi e una di cognomi
+- genero un ciclo di 6 nomi e cognomi casuali
+- li unisco
+- Stampo la lista degli invitati
--- JSnack2/main.js
+++ JSnack2/main.js
@@ -1,1 +1,20 @@
-console.log("js ok");
+//* creo una lista di nomi e una di cognomi
+const guestName = ["Luigi", "Carlo", "Enrico", "Mario", "Andrea"];
+
+const guestSurname = ["Martino", "De Luca", "De Simone", "Del Sesto", "Rossi"];
+
+//* genero un ciclo di 6 nomi e cognomi casuali
+const guestList = [];
+for (let i = 0; i < 6; i++) {
+  const casualName = guestName[Math.floor(Math.random() * guestName.length)];
+  const casualSurname =
+    guestSurname[Math.floor(Math.random() * guestSurname.length)];
+
+  //* li unisco
+  const fullName = `${casualName} ${casualSurname}`;
+  guestList.push(fullName);
+}
+
+//* Stampo la lista degli invitati
+console.log("Lista di invitati:");
+console.log(guestList);
